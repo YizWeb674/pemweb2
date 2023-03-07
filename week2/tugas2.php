f9b23d7b8e3122b15f506bfdde54aa54ad88384f
YizWeb674 <mfayizkemas@gmail.com>
2023-03-07 08:43:45 +0700

    upload tugas week2

---
+++ week2/tugas2.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Tugas 2</title>
+</head>
+<body>
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-12">
+            <h1 class="bg-warning p-3">Form Nilai Mahasiswa</h1>
+            <br>
+            <form class="form-horizontal" method="post" action="nilai_mahasiswa.php">
+                <div class="form-group row">
+                    <label for="nama" class="col-4 col-form-label">Nama Lengkap</label> 
+                    <div class="col-8">
+                    <input id="nama" name="nama" placeholder="Nama Lengkap" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="matkul" class="col-4 col-form-label">Mata Kuliah</label> 
+                    <div class="col-8">
+                    <select id="matkul" name="matkul" class="custom-select">
+                        <option value="Dasar Dasar Pemrograman">Dasar Dasar Pemrograman</option>
+                        <option value="Pemrograman Web">Pemrograman Web</option>
+                        <option value="UI/UX Design">UI/UX Design</option>
+                    </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="uts" class="col-4 col-form-label">Nilai UTS</label> 
+                    <div class="col-8">
+                    <input id="uts" name="uts" placeholder="Nilai UTS" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="uas" class="col-4 col-form-label">Nilai UAS</label> 
+                    <div class="col-8">
+                    <input id="uas" name="uas" placeholder="Nilai UAS" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="tugas" class="col-4 col-form-label">Nilai Tugas/Praktikum</label> 
+                    <div class="col-8">
+                    <input id="tugas" name="tugas" placeholder="Nilai Tugas" type="text" class="form-control">
+                    </div>
+                </div> 
+                <div class="form-group row">
+                    <div class="offset-4 col-8">
+                    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div> 
+    
+
+</body>
+</html>
